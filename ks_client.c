7aa142019830c0ad05b135ef5460237bf6232a24
Jaxsen Honeycutt <jaxsen@jxsen.com>
2025-10-31 21:19:36 -0400

    establish project foundation

--- ks_client.c
+++ ks_client.c
@@ -1,3 +1,40 @@
 //
 // Created by jax on 10/31/25.
 //
+#define MAXDIRPATH 1024
+#define MAXKEYWORD 256
+#define MAXLINESIZE 1024
+#define MAXOUTSIZE 2048
+
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3)
+    {
+        printf("Usage: ks_client <keyword> <dirpath>\n");
+        printf("Arguments expected: 2\n");
+        printf("Arguments received: %i\n", argc);
+    }
+
+    // server key
+    key_t key = ftok("ks_server.c", 67);
+    key_t client_key = ftok("ks_client.c", 67);
+
+    // test send message
+    // get msg id
+    int msgid = msgget(key, 0);
+
+    if (msgid == -1)
+    {
+        perror("msgget");
+        return(1);
+    }
+
+    char buf[1024] = "Hello World!";
+    msgsnd(msgid, &buf,sizeof(buf), 0);
+
+    return 0;
+}
--- ks_server.c
+++ ks_server.c
@@ -1,3 +1,60 @@
 //
 // Created by jax on 10/31/25.
 //
+#define MAXDIRPATH 1024
+#define MAXKEYWORD 256
+#define MAXLINESIZE 1024
+#define MAXOUTSIZE 2048
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("Starting server...\n\n");
+
+    // unioue key
+    key_t key = ftok("ks_server.c", 67);
+
+    // create or read message queue
+    int msgid = msgget(key, 0666 | IPC_CREAT);
+
+    if (msgid == -1)
+    {
+        perror("msgget");
+        return(1);
+    }
+
+    int server_status = 1;
+    char buf[1024]; // msg buffer
+    while (server_status)
+    {
+        // clear buffer
+        memset(buf, 0, sizeof(buf));
+
+        // check for msg
+        msgrcv(msgid, buf, sizeof(buf), 0, 0);
+        if (strlen(buf) > 0)
+            printf("Received message: %s\n", buf);
+        if (strcmp(buf, "quit") == 0)
+        {
+            // clean up
+            if (msgctl(msgid, IPC_RMID, 0) == -1)
+            {
+                perror("msgctl");
+                return(1);
+            }
+
+            // exit
+            return 0;
+        }
+        sleep(1);
+    }
+
+    return 0;
+}
